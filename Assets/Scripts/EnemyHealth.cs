60ef504a480172a78672d160e5c820da3b560c4a
Akash Ravindran <ar3451@nyu.edu>
2017-08-01 09:31:38 -0400

    Quick scripts. Haven't had a chance to test yet

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+
+    public int health;
+    public bool canMeleeAttack;
+    public bool canShoot;
+    public float meleeDamage;
+    public float shootDamage;
+
+    void PistolHit(int damage)
+    {
+        health = health - damage;
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public int playerCurHP;
+    public int playerMaxHP;
+
+    //public int startingAmmo;
+
+    //Need variables for bullets, shells, rockets, and cell?
+
+    // Use this for initialization
+    void Start()
+    {
+        playerCurHP = playerMaxHP;
+    }
+
+    void EnemyHit(int damage)
+    {
+        playerCurHP -= damage;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Health"))
+        {
+            playerCurHP++;
+        }
+    }
+
+    // create another for ammo (have to playtest more to see how that works)
+
+    // need to create a function for damage calculation
+}
