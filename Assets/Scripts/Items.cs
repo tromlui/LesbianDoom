a6029709b72f5792266e0bff10976d2fb2e73f4e
Akash Ravindran <ar3451@nyu.edu>
2017-08-01 13:33:41 -0400

    Tested health grabs and printed out hp

---
+++ Assets/Scripts/Items.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items : MonoBehaviour
+{
+    private float rotationSpeed = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotationSpeed);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
