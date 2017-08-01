914ace48ed09ddc283d83a8a3a3bd8c41aea9e48
Unknown <sgranoff@gm.slc.edu>
2017-08-01 13:05:26 -0400

    Auto stash before merge of "master" and "LesbianDoomGit/master"

---
+++ Assets/Scripts/EnemyTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTrigger : MonoBehaviour {
+
+	public GameObject playerTrigger;
+
+	public static bool canMove;
+
+	void OnTriggerEnter (Collider Other) {
+		if (Other.tag == "playerSphere") {
+			canMove = true;
+			//Vector3.MoveTowards (transform.position, player.transform.position, 0.3f);
+		}
+	}
+}
