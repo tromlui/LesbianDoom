20909dab875c9ff3dd72173b50555001c592c83d
Unknown <liamjkelly11@gmail.com>
2017-08-03 12:19:26 -0400

    First Prototype (edits)
    Some colors changed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,9 +10,9 @@ public class Player : MonoBehaviour
     public int playerMaxHP;
 	public Text myText;
 
-    //public int startingAmmo;
-
-    //Need variables for bullets, shells, rockets, and cell?
+	//public int startingAmmo;
+	public int ammoCount;
+	private int maxAmmo;
 
     // Use this for initialization
 
@@ -29,9 +29,18 @@ public class Player : MonoBehaviour
         if (other.gameObject.CompareTag("Health"))
         {
 			if (playerCurHP != playerMaxHP) {
-				playerCurHP++;	
+				playerCurHP++;
+				Destroy(other.gameObject);
+				Debug.Log(playerCurHP);
 			}
         }
+
+		else if (other.gameObject.CompareTag("Ammo"))
+		{
+	        ammoCount++;
+		    Destroy(other.gameObject);
+      		Debug.Log(ammoCount);
+		}
     }
 
     // create another for ammo (have to playtest more to see how that works)
