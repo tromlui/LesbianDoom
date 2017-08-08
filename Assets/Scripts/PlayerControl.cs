9eb9b245a2faa5e0137944320638fe613587342e
Unknown <liamjkelly11@gmail.com>
2017-08-08 14:45:31 -0400

    Enemy Movement Restored, Player Char Controller
    Enemy Movement does not use coroutine anymore, player movement uses a character controller. Bullet collider is deactivated after trigger collision

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 public class PlayerControl : MonoBehaviour {
 
-	public Rigidbody player; //CharacterController for player
+	public CharacterController player; //CharacterController for player
 	Vector3 inputVector;
 	public float moveSpeed;
 	public float turnSpeed;
@@ -12,10 +12,7 @@ public class PlayerControl : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		player = GetComponent<Rigidbody> ();
-		moveSpeed = 10.0f;
-		turnSpeed = 30.0f;
-		cameraSpeed = 5.0f;
+		player = GetComponent<CharacterController> ();
 	}
 	
 	// Update is called once per frame
@@ -23,16 +20,19 @@ public class PlayerControl : MonoBehaviour {
 		//w and s are forward and back
 		inputVector.z = Input.GetAxis ("Vertical") * Time.deltaTime * moveSpeed;
 
-		transform.Translate (0f, 0f, inputVector.z);
+		//transform.Translate (0f, 0f, inputVector.z);
+
 
 		//a and d are used to turn left and right
 		float turnAngle = turnSpeed * Input.GetAxis ("Horizontal") * Time.deltaTime;
 
-		transform.Rotate(0, turnAngle, 0);
 
 		//q and e are used to move to the left and the right
 		inputVector.x = Input.GetAxis ("Strafe") * Time.deltaTime * moveSpeed;
 
-		transform.Translate (inputVector.x, 0f, 0f);
+		//transform.Translate (inputVector.x, 0f, 0f);
+
+		player.Move (transform.TransformDirection (inputVector));
+		transform.Rotate(0, turnAngle, 0);
 	}
 }
