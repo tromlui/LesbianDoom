d7c6bf0a840c4352971c575f66d4aa7c77925c5d
king kule <chortletortle@gmail.com>
2017-08-08 15:24:44 -0400

    hey its a basic door i guess it opens and closes when youre in the range of the trigger ;-)

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -6,19 +6,25 @@ public class DoorScript : MonoBehaviour {
 
 	bool doorOpen = false;
 	BoxCollider doorTrigger;
+	DoorTrigger dt;
 
 	// Use this for initialization
 	void Start () {
 		doorTrigger = GetComponentInChildren<BoxCollider> ();
+		dt = GetComponentInChildren<DoorTrigger> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.R)) {
-			if (doorOpen) {
-				Debug.Log ("closing that door homes!");
+			if (!doorOpen && dt.playerInRange) {
+				Vector3 open = transform.position;
+				open.y = 6.4f;
+				transform.position = open;
 			} else {
-				Debug.Log ("woot woot open up in here");
+				Vector3 closed = transform.position;
+				closed.y = 2.33f;
+				transform.position = closed;
 			}
 			doorOpen = !doorOpen;
 		}
