1ea154e1a4aaf2a58e5fd9c37031b6ee145f7723
king kule <chortletortle@gmail.com>
2017-08-09 23:19:26 -0400

    commented all of my door code

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -4,33 +4,38 @@ using UnityEngine;
 
 public class DoorScript : MonoBehaviour {
 
+	//whether the door is open or not
 	bool doorOpen = false;
+	//if the door is either moving to close or open
 	bool isAnimating = false;
-	BoxCollider doorTrigger;
+
+	//Trigger object, used specifically to tell when in range of trigger
 	DoorTrigger dt;
+
+	//Speed of door closing
+	float doorSpeed = 1.25f;
 
 	// Use this for initialization
 	void Start () {
-		doorTrigger = GetComponentInChildren<BoxCollider> ();
 		dt = GetComponentInChildren<DoorTrigger> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//R is used as a basic use key
 		if (Input.GetKeyDown (KeyCode.R)) {
-			
+			//If the door is closed...
 			if (!doorOpen) {
+				//AND if the player is in range, the door isnt changing states
 				if (dt.playerInRange && !isAnimating) {
-					//StartCoroutine ("DoorAnimate", 4f);
+					//We animate the door to be open
 					StartCoroutine(DoorAnimate(4f));
 					doorOpen = !doorOpen;
 				}
-				/*Vector3 target = transform.position;
-				target.y = 6.4f;
-				transform.position = target;*/
 			} else {
+				//If the door is open...
 				if (!isAnimating) {
-					//StartCoroutine ("DoorAnimate", -4f);
+					//We animate the door to be closed
 					StartCoroutine(DoorAnimate(-4f));
 					doorOpen = !doorOpen;
 				}
@@ -38,28 +43,43 @@ public class DoorScript : MonoBehaviour {
 			}
 
 		}
+		//if the player isn't in range and the door is closed, and not animating...
 		if (!dt.playerInRange && doorOpen && !isAnimating) {
-			//StartCoroutine ("DoorAnimate", -4f);
+			//we close the door!
 			StartCoroutine(DoorAnimate(-4f));
 			doorOpen = !doorOpen;
 		}
 	}
 
+	//coroutines are not necessary, but I implemented it like this as a learning exercise
 	IEnumerator DoorAnimate (float y) {
+		//FLOAT y is the number of units on the y axis you want the door to move
+
 		isAnimating = true;
+		//if it is animating
+
 		float t = 0f;
+		//time
+
 		Vector3 start = transform.position;
+		//starting position
+
 		Vector3 target = new Vector3 (
 			transform.position.x,
 			transform.position.y + y,
 			transform.position.z);
+		//target position
+
+		//The main loop that allows the door the animate smoothly
 		while (t < 1) {
-			t += Time.deltaTime * 1.25f;
+			t += Time.deltaTime * doorSpeed;
+			//lerp it up baby
 			transform.position = Vector3.Lerp(start, target, t);
 
 			//0 means waiting for 1 frame
 			yield return 0;
 		}
+
 		isAnimating = false;
 	}
 }
--- Assets/Scripts/DoorTrigger.cs
+++ Assets/Scripts/DoorTrigger.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class DoorTrigger : MonoBehaviour {
 
+	//variable that is true when player is in trigger range
 	public bool playerInRange = false; 
 
 	// Use this for initialization
@@ -16,12 +17,14 @@ public class DoorTrigger : MonoBehaviour {
 		
 	}
 
+	//checks when player enters trigger range
 	void OnTriggerEnter(Collider col){
 		if (col.gameObject.tag == "player") {
 			playerInRange = true;
 		}
 	}
 
+	//checks when player exits trigger range
 	void OnTriggerExit(Collider col){
 		if (col.gameObject.tag == "player") {
 			playerInRange = false;
